865d1226f7a00a048bb51cffa2e8be627293fd5d
Shreyanshi-Vashistha <Shreyanshi-Vashistha@users.noreply.github.com>
2023-10-05 19:20:10 -0400

    updated

--- webconfig/copyWebConfig.js
+++ webconfig/copyWebConfig.js
@@ -1,9 +1,11 @@
 const fs = require("fs-extra");
+var CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 
 const webConfigPath = "./build/web.config";
 
 if (fs.existsSync(webConfigPath)) {
   fs.unlinkSync(webConfigPath);
+  new CaseSensitivePathsPlugin({ debug: true });
 }
 
 fs.copySync("./webconfig/web.config", webConfigPath);
