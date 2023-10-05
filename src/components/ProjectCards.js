ec1c7243d4c2575dd5e1c86227f585aac5d5e236
Shreyanshi-Vashistha <Shreyanshi-Vashistha@users.noreply.github.com>
2023-10-05 19:38:06 -0400

    Update ProjectCards.js

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 const ProjectCards = (props) => {
   return (
     <div className="project-card">
-      <img src={props.imageSrc} alt="image" />
+      <img src={props.imageSrc} alt="" />
       <h2 className="title">{props.title}</h2>
       <div className="details">
         <p>{props.desc}</p>
