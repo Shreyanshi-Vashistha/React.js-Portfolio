f17576eee87011b27941450b0a22819861355f2e
Shreyanshi-Vashistha <Shreyanshi-Vashistha@users.noreply.github.com>
2023-10-06 22:43:46 -0400

    Build correction

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,14 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import BgImg from "../components/BgImg";
 import Footer from "../components/Footer";
-import Work from "../components/Work";
 
 const home = () => {
   return (
     <div>
       <Navbar />
       <BgImg />
-      {/* <Work /> */}
+
       <Footer />
     </div>
   );
